c77630f76b208d6430afa4e33daaf6cdf5ef8ea7
kotala <app@kotalab.com>
2015-11-06 07:18:43 +0900

    enumを数えられるようにCaseCountableを追加

--- NotificationBarSample/NBTableViewController.swift
+++ NotificationBarSample/NBTableViewController.swift
@@ -8,9 +8,26 @@
 
 import UIKit
 
+// via:http://stackoverflow.com/questions/27094878/how-do-i-get-the-count-of-a-swift-enum
+// enum which provides a count of its cases
+protocol CaseCountable {
+    static func countCases() -> Int
+    static var caseCount : Int { get }
+}
+// provide a default implementation to count the cases for Int enums assuming starting at 0 and contiguous
+extension CaseCountable where Self : RawRepresentable, Self.RawValue == Int {
+    // count the number of cases in the enum
+    static func countCases() -> Int {
+        // starting at zero, verify whether the enum can be instantiated from the Int and increment until it cannot
+        var count = 0
+        while let _ = Self(rawValue: count) { count++ }
+        return count
+    }
+}
+
 class NBTableViewController: UITableViewController {
     
-    enum Segue: Int {
+    enum Segue: Int, CaseCountable {
         case CWSegue, JDSegue, TSSegue
         
         func toString() -> String {
@@ -20,6 +37,8 @@ class NBTableViewController: UITableViewController {
             case .TSSegue : return "TSMessage"
             }
         }
+        
+        static let caseCount = Segue.countCases()
     }
 
     override func viewDidLoad() {
@@ -34,7 +53,7 @@ class NBTableViewController: UITableViewController {
     // MARK: - Table view data source
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 3
+        return Segue.caseCount
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
